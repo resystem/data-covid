04847f31e5a159fd002f74494eb1fbcab27ba7de
Gabriel <gabrielfurlan05@gmail.com>
2020-07-02 14:23:51 -0300

    fixed icons

--- src/libs/i18n.js
+++ src/libs/i18n.js
@@ -2,11 +2,15 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import commonEN from '../locales/en/common.json';
+import commonES from '../locales/es/common.json';
 import commonPT from '../locales/pt/common.json';
 
 const resources = {
   en: {
     common: commonEN,
+  },
+  es: {
+    common: commonES,
   },
   pt: {
     common: commonPT,
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import i18n from '../libs/i18n';
 import { useTranslation } from 'react-i18next';
+import SmoothScroll from 'smooth-scroll';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import DaysWithoutMinister from '../components/DaysWithoutMinister';
 import GeneralData from '../components/GeneralData';
+import brIcon from '../images/brazil-white.svg';
+import globalIcon from '../images/global-black.svg';
 import '../components/css/home.css';
 
 /**
  * Function that returns Index project page
  */
 const IndexPage = () => {
+  const [lang, setLang] = useState('pt');
   const { t } = useTranslation();
   return (
     <Layout>
@@ -19,11 +24,35 @@ const IndexPage = () => {
           <header className="header">
             <p className="who">{t('apresentation')}</p>
             <ul className="languages">
-              <li className="language_label">{t('pt')}</li>
+              <li
+                className={lang === 'pt' ? "language_label __active" : "language_label"}               
+                onClick={() => {
+                  setLang('pt');
+                  i18n.changeLanguage('pt');
+                }}
+              >
+                {t('pt')}
+              </li>
               &nbsp;/&nbsp;
-              <li className="language_label">{t('es')}</li>
+              <li
+                className={lang === 'en' ? "language_label __active" : "language_label"} 
+                onClick={() => {
+                  setLang('en');
+                  i18n.changeLanguage('en');
+                }}
+              >
+                {t('en')}
+              </li>
               &nbsp;/&nbsp;
-              <li className="language_label">{t('en')}</li>
+              <li
+                className={lang === 'es' ? "language_label __active" : "language_label"} 
+                onClick={() => {
+                  setLang('es');
+                  i18n.changeLanguage('es');
+                }}
+              >
+                {t('es')}
+              </li>
             </ul>
           </header>
           <div className="apresentation_content">
@@ -41,7 +70,17 @@ const IndexPage = () => {
                 <br />
                 {t('page_description')}
               </p>
-              <a href="/">{t('access_information')}</a>
+              <a
+                href="/"
+                onClick={(e) => {
+                  e.preventDefault();
+                  const scroll = new SmoothScroll();
+                  const anchor = document.querySelector('#brazil-covid-data');
+                  scroll.animateScroll(anchor);
+                }}
+              >
+                {t('access_information')}
+              </a>
             </div>
           </div>
           <p className="information_source">
@@ -50,10 +89,10 @@ const IndexPage = () => {
           </p>
         </div>
       </section>
-      <section className="brazil_covid_data">
+      <section id="brazil-covid-data" className="brazil_covid_data">
         <div className="content">
           <header className="header">
-            <img src="/images/brazil-white.svg" alt="Brazil`s white icon" />
+            <img src={brIcon} alt="Brazil`s white icon" />
             <div className="title_wrapper">
               <h2 className="title">{t('current_brazil_situation')}</h2>
               <p className="updated">ATUALIZADO 23/08, ÀS 21:36</p>
@@ -66,7 +105,7 @@ const IndexPage = () => {
       <section className="global_covid_data">
         <div className="content">
           <header className="header">
-            <img src="/images/global-black.svg" alt="Global`s black icon" />
+            <img src={globalIcon} alt="Global`s black icon" />
             <div className="title_wrapper">
               <h2 className="title">{t('current_global_situation')}</h2>
               <p className="updated">ATUALIZADO 23/08, ÀS 21:36</p>
